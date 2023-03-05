2abc834504e677305fe3ed6184a1f50ca7cf02d5
Moginder <mogiegan@gmail.com>
2023-03-06 00:36:15 +0530

    changes in header files

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,29 @@
 import React from 'react';
-
+import ptulogo from './assests/ptulogo.png';
+import logotrans from './assests/logotrans.png';
 
 const Header  = ()=>{
 
     
   return (
     <div>
-    <section class="bg-[url('/src/components/assests/headerbg.jpg')] ">
-      
-        <div class="px-12 mx-auto max-w-7xl">
+    <section class="bg-[url('/src/components/assests/headerbg.jpg')] flex space-x-4">
+      <img className='max-w-lg max-h-20 md:max-h-40' src={ptulogo}/>
+        <div class=" px-12 mx-auto max-w-7xl">
           <div class="w-full mx-auto text-left md:w-11/12 xl:w-9/12 md:text-center">
-            <h1 class="mb-8 text-2xl text-center font-extrabold leading-none tracking-normal text-slate-800 md:text-3xl md:tracking-tight">
+            <h1 class="mb-8 text-lg md:text-2xl text-center font-extrabold leading-none tracking-normal text-slate-800 md:text-3xl md:tracking-tight">
               <span className="dark:text-white">INTERNATIONAL CONFERENCE ON ADVANCES IN
 COMPUTATIONAL INTELLIGENCE IN COMMUNICATION</span>
             </h1>
-            <h1 className='my-10 text-center text-2xl font-black leading-none tracking-normal text-slate-800 md:text-xl md:tracking-tight'><span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-white lg:inline  ">CIC 2016
+            <h1 className='my-10 text-center text-lg md:text-2xl font-black leading-none tracking-normal text-slate-800 md:text-xl md:tracking-tight'><span className="block w-full py-2 text-transparent bg-clip-text leading-12 bg-white lg:inline  ">CIC 2016
 Pondicherry Engineering College, Puducherry , India</span></h1>
-            {/* <p class="px-0 mb-8 text-lg text-gray-600 md:text-xl lg:px-24">
-              
-            </p> */}
+            
             <div class="mb-0 space-x-0 md:space-x-2 md:mb-8">
 
             </div>
           </div>
         </div>
+        <img className='max-w-md md:max-h-40 max-h-20' src={logotrans}/>
 
       
     </section >
--- tailwind.config.js
+++ tailwind.config.js
@@ -29,6 +29,20 @@ module.exports = {
       dropShadow: {
         primary: ' 0px 5px 5px rgba(75, 93, 104, 0.1)',
       },
+      animation: {
+        marquee: 'marquee 25s linear infinite',
+        marquee2: 'marquee2 25s linear infinite',
+      },
+      keyframes: {
+        marquee: {
+          '0%': { transform: 'translateX(0%)' },
+          '100%': { transform: 'translateX(-100%)' },
+        },
+        marquee2: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0%)' },
+        },
+      },
     },
   },
   plugins: [],
